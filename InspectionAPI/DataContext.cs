5838789f1ea7c71b5105d43bebdb5f321ca4d132
Luiggi <Luiggi@DESKTOP-PAAP7Q8>
2022-12-18 20:38:02 -0500

    Agregar archivos de proyecto.

---
+++ InspectionAPI/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace InspectionAPI
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statutes { get; set; }
+    }
+}
